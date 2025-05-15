deec91fa6482d90ec03b96005bed0a533f93bc61
Alex Brosseau <brosseau.alex@outlook.com>
2025-05-15 00:38:01 -0400

    do it for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component').then(c => c.HomeComponent),
+        title: 'Home'
+      },
+      {
+        path: 'projects',
+        loadComponent: () =>
+          import('./pages/about/about.component').then(c => c.AboutComponent),
+        title: 'Projects'
+      },
+];
